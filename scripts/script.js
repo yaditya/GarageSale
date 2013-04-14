e339a5d766dfb6f78bc0cf3129e03a9e05414a06
Yovi B <yovi@yovib.com.au>
2013-04-14 17:13:33 +1000

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+$(function() {
+	$('.gallery').colorbox({
+		scrolling: false
+	});
+	$('.coco').colorbox({rel:'coco'});
+	$('.mwave').colorbox({rel:'mwave'});
+	$('.wd').colorbox({rel:'wd'});
+	$('.tablet').colorbox({rel:'tablet'});
+	$('.tv').colorbox({rel:'tv'});
+	$('.fan').colorbox({rel:'fan'});
+	$('.oil').colorbox({rel:'oil'});
+	$('.fp').colorbox({rel:'fp'});
+	$('.tallboy').colorbox({rel:'tallboy'});
+	$('.qb').colorbox({rel:'qb'});
+	$('.kb').colorbox({rel:'kb'});
+	$('.ht').colorbox({rel:'ht'});
+	$('.bfast').colorbox({rel:'bfast'});
+	$('.besta').colorbox({rel:'besta'});
+	$('.circulartbl').colorbox({rel:'circulartbl'});
+	$('.scanpan').colorbox({rel:'scanpan'});
+	$('.dryer').colorbox({rel:'dryer'});
+	$('.aspen').colorbox({rel:'aspen'});
+	$('.owchair').colorbox({rel:'owchair'});
+	$('.bass').colorbox({rel:'bass'});
+	$('.cube').colorbox({rel:'cube'});
+});
+
+
+
+
+
